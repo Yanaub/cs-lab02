74e9dbd3785a10e0a9aa4793441bb8887d8625b2
Alice (UbushaevaYA) <yana.ubushaeva@gmail.com>
2023-07-04 01:11:51 +0300

    Сохранение данных из сети в буфер

--- main.cpp
+++ main.cpp
@@ -61,7 +61,7 @@ Input read_input(istream& in,bool prompt) {
 
 
 
-void removing_repetitions(Input input){
+void removing_repetitions(Input &input){
     for(int i=0;i<input.numbers.size();i++){
         for(int j=i+1;j<input.numbers.size();j++){
             if(input.numbers[i]==input.numbers[j]){
@@ -70,6 +70,15 @@ void removing_repetitions(Input input){
             }
         }
     }
+
+}
+size_t write_data(void* items, size_t item_size, size_t item_count, void* ctx) {
+    stringstream* buffer = reinterpret_cast<stringstream*>(ctx);
+    size_t data_size=item_size*item_count;
+    const char* it = reinterpret_cast<const char*>(items);
+    buffer->write(it, data_size);
+
+return 0;
 }
 
 Input download(const string& address) {
@@ -78,9 +87,15 @@ CURL* curl = curl_easy_init();
             if(curl) {
                     CURLcode res;
             curl_easy_setopt(curl, CURLOPT_URL, address);
+            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);
             res = curl_easy_perform(curl);
             if (res!=CURLE_OK){
-                cout<< curl_easy_strerror(res);
+                cerr<< curl_easy_strerror(res);
+                char *ct;
+                cerr<<curl_easy_getinfo(curl, CURLINFO_CONTENT_TYPE, &ct);
+
+
                 exit(1);
             }
             curl_easy_cleanup(curl);}
@@ -89,22 +104,25 @@ CURL* curl = curl_easy_init();
 
 
 
+
 int main(int argc, char* argv[]) {
 
     Input input;
-    if (argc > 1) {
+    if (argc > 1) {cerr<<"***********";
             input = download(argv[1]);
-    } else {
+
+    } else {cerr<<"88888888";
         input = read_input(cin, true);
+
 }
 
 
     curl_global_init(CURL_GLOBAL_ALL);
 
 
-
     removing_repetitions(input);
-    double min, max;
+
+        double min, max;
     find_minmax(input.numbers, min, max);
 
     const auto bins = make_histogram(input);
